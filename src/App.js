07a09c6289efa4b83a0ab00a9dc43355c1aaeb58
thanghv <thanghv@hblab.vn>
2020-05-11 19:53:39 +0700

    update code todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
 import TodoItem from './components/TodoItem';
 import './App.css'
+import checkAll from './components/img/down-arrow.svg'
 
 class App extends Component {
   constructor() {
     super();
-    this.todoItems = [
-      { title: 'Wake up', isComplete: true },
-      { title: 'Go to home', isComplete: false },
-      { title: 'Learn', isComplete: false },
-    ]
+    this.state = {
+      todoItems: [
+        { title: 'Wake up', isComplete: true },
+        { title: 'Go to home', isComplete: false },
+        { title: 'Learn', isComplete: false },
+      ]
+    }
+  }
+
+  onItemClicked(item) {
+    return (event) => {
+      const indexItem = this.state.todoItems.findIndex(todoItem => todoItem.title === item.title)
+      const isComplete = item.isComplete
+      const { todoItems } = this.state
+      const cloneObj = JSON.parse(JSON.stringify(todoItems));
+      console.log(cloneObj, 'Deep clone')
+      // log todoItems before change
+      console.log(isComplete, todoItems, indexItem)
+      //  change todoItems arry
+      todoItems[indexItem].isComplete = !isComplete
+      // log todoItems after change
+      console.log(isComplete, todoItems)
+      this.setState({
+        todoItems: todoItems
+      })
+    }
   }
 
   render() {
+    const { todoItems } = this.state
     return (
       <div className="App">
-        {
-          this.todoItems.map((item, index) =>
-            <TodoItem key={index} item={item} />
-          )
-        }
+        <header>
+          {/* <h1> Hello </h1> */}
+          <img src={checkAll} alt=""/>
+          <input placeholder="What needs to be done" type="text"/>
+        </header>
+        <ul>
+          {
+            todoItems.map((item, index) =>
+              <TodoItem key={index} item={item} onClick={this.onItemClicked(item)} />
+            )
+          }
+        </ul>
       </div>
     )
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import './TodoItem.css'
+import './TodoItem.css';
 import classNames from 'classnames';
+import check from './img/check.svg';
+import verified from './img/verified.svg';
+
 class TodoItem extends Component {
     render() {
-        const { item } = this.props;
-        let className = classNames('todo-item', {'todo-complete' : item.isComplete})
+        const { item, onClick } = this.props;
+        let url = verified;
+        if(item.isComplete) {
+            url = check;
+        }
+        let className = classNames({'todo-complete': item.isComplete})
         return (
-            <div className={ className }>
-                <p> { item.title } </p>
-            </div>
-        )
+            <li onClick={onClick} className="todo-item">
+                <div className="view">
+                    <img className="check-img" src={url} alt="" />
+                    <span className={className}> {item.title} </span>
+                </div>
+            </li>
+        );
     }
 }
 
